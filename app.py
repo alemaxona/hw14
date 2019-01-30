8fb32de22be8cc7187f3ceef434b4247abe59644
Max <alemaxona@bk.ru>
2019-01-30 14:59:05 +0300

    Fixing

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ from config import Configuration
 app = Flask(__name__, template_folder='templates')
 app.config.from_object(Configuration)
 db = SQLAlchemy(app)
+# print(dir(db))
 
 
 # !!!
@@ -26,24 +27,28 @@ def index():
             user_post = Article(**form.data)
             db.session.add(user_post)
             db.session.commit()
-
             posts = db.session.query(Article).all()
+            db.session.close()
             return render_template('Article.html', posts=posts)
         else:
             return 'Form not valid.'
     else:
         if db.session.query(Article).count() == 0:
+            db.session.close()
             return 'Posts not found.'
         else:
-            posts = Article.query.all()
-            # for column in posts:
-            #     author = column.author
-            #     title = column.title
-            #     print(author, title)
+            posts = db.session.query(Article).all()  # OR: posts = Article.query.all()
+            # print('???', posts)
+            # ??? [<Article 2>, <Article 3>, <Article 4>, <Article 5>, <Article 6>, <Article 7>, <Article 8>, <Article 9>, <Article 10>]
+            
+            # posts = db.session.execute("SELECT * FROM article")
+            # print('???', posts)
+            # ??? <sqlalchemy.engine.result.ResultProxy object at 0x047CB470>
+            db.session.close() 
             return render_template('Article.html', posts=posts)
 
 
-@app.route('/<post_id>', methods=['GET', 'POST'])
+@app.route('/<int:post_id>', methods=['GET', 'POST'])
 def comments(post_id):
 
     from form import ArticleForm, CommentForm
@@ -51,22 +56,30 @@ def comments(post_id):
 
     if request.method == 'GET':
         posts = db.session.query(Article).filter_by(id=post_id)
-        print('!!!!!!',posts)
-        # if posts.count() == 0:
-        #     return 'Post not found'
-        # else:
-        # print('posts', posts)  # ? Может выводиться запрос SQL
-        comments = db.session.query(Comment.text).join(Article).filter(Article.id == Comment.article_id, ).filter_by(id=post_id)
-        # print('comments', comments)
-        return render_template('user_post.html', posts=posts, comments=comments)
+        # print('???', posts)
+        # ??? SELECT article.id AS article_id, article.date_create AS article_date_create, article.author AS article_author, article.title AS article_title, article.post AS article_post
+        # FROM article WHERE article.id = %(id_1)s
+
+        # posts = db.session.execute("Select * from article where id = :post_id")
+        if posts.count() == 0:
+            db.session.close()  # Работает 
+            # db.session.remove()  # Тут это работает! O_o Все это блять страннно! Помогите мне, тупому.
+            return 'Post not found'
+        else:
+            comments = db.session.query(Comment).join(Article).filter(Article.id == Comment.article_id).filter_by(id=post_id)
+            # print('???', comments)
+            # SELECT comment.id AS comment_id, comment.article_id AS comment_article_id, comment.date_create AS comment_date_create, comment.text AS comment_text
+            # FROM comment JOIN article ON article.id = comment.article_id
+            # WHERE article.id = comment.article_id AND article.id = %(id_1)s
+            db.session.close()  # Не работает
+            return render_template('user_post.html', posts=posts, comments=comments)
     else:
         form = CommentForm(request.form)
         if form.validate():
             user_comment = Comment(**form.data)
             # print(request.form.get('article_id'))  # Получаем данные воода
             if request.form.get('article_id') == post_id:
-                db.session.add(user_comment)
-                db.session.commit() 
+                db.session.add(user_comment) 
                 posts = db.session.query(Article).filter_by(id=post_id)
                 comments = db.session.query(Comment.text).join(Article).filter(Article.id == Comment.article_id).filter_by(id=post_id)
                 return render_template('user_post.html', posts=posts, comments=comments)
